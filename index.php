4c03d5235b214fcb3a40b2b363f96fdf91946749
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-11-14 16:57:57 +0600

    init

--- index.php
+++ index.php
@@ -13,12 +13,12 @@
 
 <body>
     <nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-white portfolio-navbar gradient">
-        <div class="container"><a class="navbar-brand logo" href="index.html">Ashraf Kabir</a>
+        <div class="container"><a class="navbar-brand logo" href="index.php">Ashraf Kabir</a>
             <button data-toggle="collapse" class="navbar-toggler" data-target="#navbarNav">
                 <span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="nav navbar-nav ml-auto">
-                    <li class="nav-item" role="presentation"><a class="nav-link active" href="index.html">Home</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link active" href="index.php">Home</a></li>
                     <li class="nav-item" role="presentation"><a class="nav-link" href="projects.html">Projects</a></li>
                     <li class="nav-item" role="presentation"><a class="nav-link" href="cv.html">CV</a></li>
                     <li class="nav-item" role="presentation"><a class="nav-link" href="contact.html">Contact</a></li>
@@ -134,7 +134,7 @@
                                                                 style="background-color: rgb(0,0,0);" target="_blank"><i
                     class="icon ion-social-github"></i></a>
                 <a href="https://twitter.com/ashraf1Q95" style="background-color: rgb(29,161,242);"
-                        target="_blank"><i class="icon ion-social-twitter"></i></a>
+                   target="_blank"><i class="icon ion-social-twitter"></i></a>
             </div>
         </div>
     </footer>
