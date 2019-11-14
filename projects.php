14dc81b964ab97fb4a94232f395cf3c8cc22919e
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-11-14 20:49:09 +0600

    updates

--- projects.php
+++ projects.php
@@ -14,23 +14,9 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-white portfolio-navbar gradient">
-        <div class="container"><a class="navbar-brand logo" href="index.php">Ashraf Kabir</a>
-            <button data-toggle="collapse" class="navbar-toggler" data-target="#navbarNav"><span class="sr-only">Toggle navigation</span><span
-                        class="navbar-toggler-icon"></span></button>
-            <div
-                    class="collapse navbar-collapse" id="navbarNav">
-                <ul class="nav navbar-nav ml-auto">
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="index.php">Home</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link active" href="projects.php">Projects</a>
-                    </li>
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="cv.php">CV</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="contact.php">Contact</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="hire-me.php">Hire me</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <!-- header -->
+    <?php include "includes/header.php"; ?>
+    
     <main class="page projects-page">
         <section class="portfolio-block projects-cards">
             <div class="container" data-aos="zoom-in" data-aos-duration="1000">
