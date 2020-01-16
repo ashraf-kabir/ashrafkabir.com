23c64bec2d69976fa5188cf4500cb87dd9247232
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-01-16 15:03:04 +0600

    updates

--- includes/footer.php
+++ includes/footer.php
@@ -1,16 +1,20 @@
 <footer class="page-footer">
     <div class="container">
-        <div class="links"><a href="cv.php">About me</a><a href="contact.php">Contact me</a><a href="projects.php">Projects</a>
+        <div class="links">
+            <a href="cv.php" style="text-decoration: none; font-weight: bold;">About me</a>
+            <a href="contact.php" style="text-decoration: none; font-weight: bold;"">Contact me</a>
+            <a href="projects.php" style="text-decoration: none; font-weight: bold;"">Projects</a>
         </div>
-        <div class="social-icons"><a style="background-color: rgb(66,103,178);" target="_blank"
-                                     href="https://www.facebook.com/sonnet404"><i
-                    class="icon ion-social-facebook"></i></a><a href="https://github.com/ashraf-kabir/"
-                                                                style="background-color: rgb(0,0,0);"
-                                                                target="_blank"><i
-                    class="icon ion-social-github"></i></a>
-            <a
-                href="https://twitter.com/ashraf1Q95" style="background-color: rgb(29,161,242);"
-                target="_blank"><i class="icon ion-social-twitter"></i></a>
+        <div class="social-icons">
+            <a href="https://www.facebook.com/sonnet404" style="background-color: rgb(66,103,178);" target="_blank">
+                <i class="icon ion-social-facebook"></i>
+            </a>
+            <a href="https://github.com/ashraf-kabir/" style="background-color: rgb(0,0,0);" target="_blank">
+                <i class="icon ion-social-github"></i>
+            </a>
+            <a href="https://twitter.com/ashraf1Q95" style="background-color: rgb(29,161,242);" target="_blank">
+                <i class="icon ion-social-twitter"></i>
+            </a>
         </div>
     </div>
 </footer>
--- includes/header.php
+++ includes/header.php
@@ -1,17 +1,30 @@
 <nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-white portfolio-navbar gradient">
     <div class="container"><a class="navbar-brand logo" href="index.php">Ashraf Kabir</a>
-        <button data-toggle="collapse" class="navbar-toggler" data-target="#navbarNav"><span class="sr-only">Toggle navigation</span><span
-                class="navbar-toggler-icon"></span></button>
+        <button data-toggle="collapse" class="navbar-toggler" data-target="#navbarNav">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="navbar-toggler-icon"></span>
+        </button>
         <div
             class="collapse navbar-collapse" id="navbarNav">
             <ul class="nav navbar-nav ml-auto">
-                <li class="nav-item" role="presentation"><a class="nav-link active" href="index.php">Home</a></li>
-                <li class="nav-item" role="presentation"><a class="nav-link" href="http://blog.ashrafkabir.com/"
-                                                            target="_blank">Blog</a></li>
-                <li class="nav-item" role="presentation"><a class="nav-link" href="projects.php">Projects</a></li>
-                <li class="nav-item" role="presentation"><a class="nav-link" href="cv.php">CV</a></li>
-                <li class="nav-item" role="presentation"><a class="nav-link" href="contact.php">Contact</a></li>
-                <li class="nav-item" role="presentation"><a class="nav-link" href="hire-me.php">Hire me</a></li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link active" href="index.php">Home</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" href="http://blog.ashrafkabir.com/" target="_blank">Blog</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" href="projects.php">Projects</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" href="cv.php">CV</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" href="contact.php">Contact</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" href="hire-me.php">Hire me</a>
+                </li>
             </ul>
         </div>
     </div>
